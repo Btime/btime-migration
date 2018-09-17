5f11f40267cdff48cded9c4058cf3ac5bd02ce84
Jhonatan Amorim <amorim.dev@gmail.com>
2018-09-17 10:27:47 -0300

    generate files command

---
+++ src/generate.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const moment = require('moment')
+const yargs = require('yargs')
+const typeOptions = [ 'sql', 'nonsql' ]
+
+module.exports.generate = () => {
+  return new Promise((resolve, reject) => {
+    try {
+      const argv = yargs
+        .usage('Usage: $0 -t [sql or nonsql]')
+        .option('type', {
+          alias: 't',
+          describe: 'Relational (sql) or NonRelational (nonsql)',
+          type: 'string'
+        })
+        .demandOption([ 'type' ])
+        .argv
+
+      if (typeOptions.indexOf(argv.type) === -1) {
+        throw new Error('Invalid type format')
+      }
+
+      const fileExtension = argv.type === 'nonsql' ? '.json' : '.sql'
+      const file = path.join(
+        __dirname,
+        '..',
+        'migrations',
+        argv.t,
+        'Version' + moment().format('YYYYMMDDHHmmssSSS') + fileExtension
+      )
+
+      fs.appendFile(file, '', function (err) {
+        if (err) throw err
+
+        resolve(file)
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
---
+++ test/test.js
@@ -0,0 +1,59 @@
+/* eslint-env mocha */
+
+const chai = require('chai')
+const expect = chai.expect
+const { exec } = require('child_process')
+const path = require('path')
+const generate = path.join(__dirname, '../bin/generate')
+
+describe('Generate tests', () => {
+  it('Expect to not generate file migration', (done) => {
+    exec(generate, (error, stdout, stderr) => {
+      expect(error).to.not.be.equal(null)
+      expect(stdout.length).to.be.equal(0)
+      expect(stderr.length).to.not.be.equal(0)
+      done(null)
+    })
+  })
+
+  it('Expect to not generate invalid file migration', (done) => {
+    exec(generate + ' -t invalid', (error, stdout, stderr) => {
+      const invalidErrorMessage = '\u001b[31mError: Invalid type format\u001b[0m\n'
+      expect(error).to.be.equal(null)
+      expect(stdout.length).to.not.be.equal(0)
+      expect(stdout).to.be.equal(invalidErrorMessage)
+      expect(stderr.length).to.be.equal(0)
+      done(null)
+    })
+  })
+
+  it('Expect to generate sql file migration', (done) => {
+    exec(generate + ' -t sql', (error, stdout, stderr) => {
+      expect(error).to.be.equal(null)
+      expect(stdout.length).to.not.be.equal(0)
+      expect(stderr.length).to.be.equal(0)
+
+      const expectFileFormat = '.sql'
+      const parts = stdout.split('/')
+      expect(parts[parts.length - 1].indexOf(expectFileFormat))
+        .to.not.be.equal(-1)
+
+      done(null)
+    })
+  })
+
+  it('Expect to generate nonsql file migration', (done) => {
+    exec(generate + ' -t nonsql', (error, stdout, stderr) => {
+      expect(error).to.be.equal(null)
+      expect(stdout.length).to.not.be.equal(0)
+      expect(stderr.length).to.be.equal(0)
+
+      const expectFileFormat = '.json'
+      const parts = stdout.split('/')
+      expect(parts[parts.length - 1].indexOf(expectFileFormat))
+        .to.not.be.equal(-1)
+
+      done(null)
+    })
+  })
+})
