95e2aadc59eabf42b268ba92d9dbd172085027cd
Guilherme Maia <guilhermecmaia.io@gmail.com>
2018-09-21 17:19:53 -0300

    Create connection URI parser to identify database type (SQL or NonSQL)

---
+++ src/migrate/uri-parser.js
@@ -0,0 +1,37 @@
+const SUPPORTED_DRIVERS = {
+  'sql': [
+    'postgres',
+    'mysql',
+    'sqlite',
+    'mssql'
+  ],
+  'nonsql': [ 'mongodb' ]
+}
+
+module.exports.parse = (uri) => {
+  const regexDriversPattern = [].concat(
+    SUPPORTED_DRIVERS.sql,
+    SUPPORTED_DRIVERS.nonsql
+  ).join('|')
+
+  const driver = RegExp(`^(${regexDriversPattern})`).exec(uri)
+
+  if (!driver) {
+    throw new Error(
+      `The connection ${uri} could not be resolved as SQL or NONSQL`
+    )
+  }
+  return driverType(driver.shift())
+}
+
+function driverType (driver) {
+  if (SUPPORTED_DRIVERS.sql.includes(driver)) {
+    return 'sql'
+  }
+
+  if (SUPPORTED_DRIVERS.nonsql.includes(driver)) {
+    return 'nonsql'
+  }
+
+  throw new Error(`${driver} is not supported.`)
+}
