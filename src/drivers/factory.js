7d71f926a8c9301412b4c0240754327a5ac93225
Guilherme Maia <guilhermecmaia.io@gmail.com>
2018-09-24 16:17:17 -0300

    Create database driver instance by type (SQL or NonSQL)

--- src/drivers/factory.js
+++ src/drivers/factory.js
@@ -8,8 +8,26 @@ module.exports.make = (uri) => {
       return require('./sql')
     }
 
-    throw new Error('Could not resolve driver from connection URI')
+    throw new Error(`Could not resolve driver from connection URI: ${uri}`)
   } catch (error) {
     throw error
   }
+}
+
+module.exports.makeByType = (payload) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const type = payload.connection.type
+
+      if (type === 'sql') {
+        return resolve(Object.assign(payload, {
+          driver: require('./sql')
+        }))
+      }
+
+      throw new Error(`Could not resolve driver for type: ${type}`)
+    } catch (error) {
+      return reject(error)
+    }
+  })
 }
