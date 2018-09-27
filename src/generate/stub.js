0404401f4ec15d897f563cf25c76817598b4958e
Guilherme Maia <guilhermecmaia.io@gmail.com>
2018-09-27 14:33:06 -0300

    Fix migration stub to prevent promise chain from breaking

--- src/generate/stub.js
+++ src/generate/stub.js
@@ -1,11 +1,11 @@
 module.exports.up = (connection) => {
   return new Promise((resolve, reject) => {
-    resolve(__filename)
+    return resolve(__filename)
   })
 }
 
 module.exports.down = (payload) => {
   return new Promise((resolve, reject) => {
-    resolve({ ...payload })
+    return resolve({ ...payload })
   })
 }
