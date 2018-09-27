46041a221f5e08ca4dce766762bf513ffccd5885
Guilherme Maia <guilhermecmaia.io@gmail.com>
2018-09-27 14:38:55 -0300

    Create separate test suite for environment

--- test/migrate.test.js
+++ test/migrate.test.js
@@ -10,22 +10,6 @@ const migrate = path.join(__dirname, '..', 'bin/migrate')
 const Filename = require('./../src/filename')
 
 describe('Migrate tests', () => {
-  it('Expect environment file (.env) to exist', (done) => {
-    fs.access(path.join(__dirname, '..', '.env'), fs.constants.F_OK, (err) => {
-      expect(err).to.equal(null)
-      done(null)
-    })
-  })
-
-  it('Expect required environment variables to be set', (done) => {
-    expect(process.env.SQL_URI.length).to.be.gt(0)
-    expect(process.env.NONSQL_URI.length).to.be.gt(0)
-    expect(process.env.MULTIPLE_URI.length).to.be.gt(0)
-    expect(process.env.MULTIPLE_COLLECTION.length).to.be.gt(0)
-    expect(process.env.MULTIPLE_COLLECTION_COLUMN.length).to.be.gt(0)
-    done(null)
-  })
-
   it('Expect failure without required arguments', (done) => {
     exec(`${migrate}`, (err, stdout, stderr) => {
       expect(err).to.not.equal(null)
