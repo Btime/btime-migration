d1c76aa8a2af18dd85c44f44da2138e0f581c6a7
Jhonatan Amorim <amorim.dev@gmail.com>
2018-10-03 15:09:58 -0300

    Update environment.test.js

--- test/environment.test.js
+++ test/environment.test.js
@@ -1,17 +1,9 @@
 /* eslint-env mocha */
 
-const fs = require('fs')
-const path = require('path')
+require('dotenv').config()
 const { expect } = require('chai')
 
 describe('Environment tests', () => {
-  it('Expect environment file (.env) to exist', (done) => {
-    fs.access(path.join(__dirname, '..', '.env'), fs.constants.F_OK, (err) => {
-      expect(err).to.equal(null)
-      done(null)
-    })
-  })
-
   it('Expect required environment variables to be set', (done) => {
     expect(process.env.SQL_URI.length).to.be.gt(0)
     expect(process.env.NONSQL_URI.length).to.be.gt(0)
