ad72f5ea5c6da8a2b5230bd4ff5a692c872197f8
Guilherme Maia <guilhermecmaia.io@gmail.com>
2018-10-18 11:42:45 -0300

    Create migrations to remove CPF/CNPJ uniqueness and add "default" column to company

---
+++ migrations/sql/Version20181018113335613.js
@@ -0,0 +1,27 @@
+module.exports.up = (payload) => {
+  return new Promise((resolve, reject) => {
+    const query = `
+    ALTER TABLE public."company"
+    ADD COLUMN "default" BOOLEAN NOT NULL DEFAULT false;
+    `
+
+    return payload.connection.instance
+      .query(query)
+      .then(() => resolve(payload))
+      .catch(reject)
+  })
+}
+
+module.exports.down = (payload) => {
+  return new Promise((resolve, reject) => {
+    const query = `
+    ALTER TABLE public."company"
+    DROP COLUMN "default";
+    `
+
+    return payload.connection.instance
+      .query(query)
+      .then(() => resolve(payload))
+      .catch(reject)
+  })
+}
