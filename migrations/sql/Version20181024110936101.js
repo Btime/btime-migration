48f37b3a5dd66956142076e03f286c661b5231af
Guilherme Maia <guilhermecmaia.io@gmail.com>
2018-10-24 11:23:57 -0300

    Create relationship between service order and company

---
+++ migrations/sql/Version20181024110936101.js
@@ -0,0 +1,27 @@
+module.exports.up = (payload) => {
+  return new Promise((resolve, reject) => {
+    const query = `
+    ALTER TABLE public."serviceOrder"
+    ADD COLUMN "companyId" INTEGER DEFAULT NULL;
+    `
+
+    return payload.connection.instance
+      .query(query)
+      .then(() => resolve(payload))
+      .catch(reject)
+  })
+}
+
+module.exports.down = (payload) => {
+  return new Promise((resolve, reject) => {
+    const query = `
+    ALTER TABLE public."serviceOrder"
+    DROP COLUMN IF EXISTS "companyId";
+    `
+
+    return payload.connection.instance
+      .query(query)
+      .then(() => resolve(payload))
+      .catch(reject)
+  })
+}
