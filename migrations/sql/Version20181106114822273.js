eaa1a1c60abd33966bbe0b4526a0c10c5b65caa9
Fellipe Capelli <fellipecapelli@gmail.com>
2018-11-06 11:57:31 -0200

    Add user group migration

---
+++ migrations/sql/Version20181106114822273.js
@@ -0,0 +1,30 @@
+module.exports.up = (payload) => {
+  return new Promise((resolve, reject) => {
+    const query = `
+    DROP TABLE public."userGroupAssociation";
+
+    ALTER TABLE public."user"
+    ADD COLUMN IF NOT EXISTS "groupId" INTEGER;
+
+    ALTER TABLE public."user"
+      ADD CONSTRAINT "user_groupId_fkey" FOREIGN KEY ("groupId")
+      REFERENCES public."minicrud" (id) ON UPDATE CASCADE ON DELETE SET NULL;
+    `
+
+    return payload.connection.instance
+      .query(query)
+      .then(() => resolve(payload))
+      .catch(reject)
+  })
+}
+
+module.exports.down = (payload) => {
+  return new Promise((resolve, reject) => {
+    const query = ``
+
+    return payload.connection.instance
+      .query(query)
+      .then(() => resolve(payload))
+      .catch(reject)
+  })
+}
