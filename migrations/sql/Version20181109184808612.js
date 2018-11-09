e5b4e87fb12a4a5fb4fc46f29a6d9049663175ba
Fellipe Capelli <fellipecapelli@gmail.com>
2018-11-09 18:50:26 -0200

    Add migration to remove all minicrud tables

---
+++ migrations/sql/Version20181109184808612.js
@@ -0,0 +1,44 @@
+module.exports.up = (payload) => {
+  return new Promise((resolve, reject) => {
+    const query = `
+    DROP TABLE public."approved";
+    DROP TABLE public."canceled";
+    DROP TABLE public."checkListCategory";
+    DROP TABLE public."customerGroup";
+    DROP TABLE public."customerSegment";
+    DROP TABLE public."fleetBrand";
+    DROP TABLE public."fleetClass";
+    DROP TABLE public."fleetCompany";
+    DROP TABLE public."fleetContract";
+    DROP TABLE public."fleetType";
+    DROP TABLE public."paymentType";
+    DROP TABLE public."pending";
+    DROP TABLE public."refundType";
+    DROP TABLE public."refused";
+    DROP TABLE public."requesterDepartment";
+    DROP TABLE public."requesterRole";
+    DROP TABLE public."rescheduling";
+    DROP TABLE public."serviceTransport";
+    DROP TABLE public."serviceType";
+    DROP TABLE public."unproductive";
+    DROP TABLE public."userGroup";
+    DROP TABLE public."validated";
+    `
+
+    return payload.connection.instance
+      .query(query)
+      .then(() => resolve(payload))
+      .catch(reject)
+  })
+}
+
+module.exports.down = (payload) => {
+  return new Promise((resolve, reject) => {
+    const query = ``
+
+    return payload.connection.instance
+      .query(query)
+      .then(() => resolve(payload))
+      .catch(reject)
+  })
+}
